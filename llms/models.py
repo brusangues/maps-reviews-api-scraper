f4346724f630d4946d0caefbca6efe6090cb9dfd
Bruno S Rodrigues <34436334+brusangues@users.noreply.github.com>
2025-03-01 00:27:52 -0300

    integrações de api grátis ok; g-eval

--- llms/models.py
+++ llms/models.py
@@ -6,28 +6,41 @@ import transformers
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_huggingface import HuggingFacePipeline
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+from langchain_openai import ChatOpenAI
 from langchain_core.outputs import Generation, LLMResult
 
 from analysis.src.utils import timeit
 
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
 
-# transformers.logging.set_verbosity_debug()
-# logging.basicConfig(level=logging.DEBUG)
-# set_debug(True)
+MAX_NEW_TOKENS = 3000
 
 
+# fmt: off
 models_text = {
-    "llama1b": "unsloth/Llama-3.2-1B-Instruct-bnb-4bit",
-    "llama3b": "unsloth/Llama-3.2-3B-Instruct-bnb-4bit",
-    "gemma2b": "unsloth/gemma-2-2b-it-bnb-4bit",
-    "phi": "unsloth/Phi-3.5-mini-instruct-bnb-4bit",
-    "gemini-2.0-flash": "gemini-2.0-flash",
-    "gemini-2.0-flash-lite": "gemini-2.0-flash-lite-preview-02-05",
-    "gemini-1.5-flash": "gemini-1.5-flash",
-    "gemini-1.5-flash-8b": "gemini-1.5-flash-8b",
-    "gemini-1.5-pro": "gemini-1.5-pro",
+    "llama1b": ("local", "unsloth/Llama-3.2-1B-Instruct-bnb-4bit"),
+    "llama3b": ("local", "unsloth/Llama-3.2-3B-Instruct-bnb-4bit"),
+    "gemma2b": ("local", "unsloth/gemma-2-2b-it-bnb-4bit"),
+    "phi":     ("local", "unsloth/Phi-3.5-mini-instruct-bnb-4bit"),
+
+    "gemini-2.0-flash":      ("google", "gemini-2.0-flash"),
+    "gemini-2.0-flash-lite": ("google", "gemini-2.0-flash-lite"),
+    "gemini-1.5-flash":      ("google", "gemini-1.5-flash"),
+    "gemini-1.5-flash-8b":   ("google", "gemini-1.5-flash-8b"),
+    "gemini-1.5-pro":        ("google", "gemini-1.5-pro"),
+
+    "gemini-2.0-pro": ("open_router", "google/gemini-2.0-pro-exp-02-05:free", "https://openrouter.ai/api/v1"),
+    "gemini-2.0-flash-thinking": ("open_router", "google/gemini-2.0-flash-thinking-exp:free", "https://openrouter.ai/api/v1"),
+    "or-llama70b":    ("open_router", "meta-llama/llama-3.3-70b-instruct:free", "https://openrouter.ai/api/v1"),
+    "or-r1":          ("open_router", "deepseek/deepseek-r1:free", "https://openrouter.ai/api/v1"),
+    "qwen2.5":        ("open_router", "qwen/qwen2.5-vl-72b-instruct:free", "https://openrouter.ai/api/v1"),
+
+    "gpt-4o":      ("github", "gpt-4o", "https://models.inference.ai.azure.com"),
+    "gpt-4o-mini": ("github", "gpt-4o-mini", "https://models.inference.ai.azure.com"),
+    "gh-r1":       ("github", "DeepSeek-R1", "https://models.inference.ai.azure.com"),
+    "gh-llama70b": ("github", "Llama-3.3-70B-Instruct", "https://models.inference.ai.azure.com"),
 }
+# fmt: on
 models_embedding = {
     "gte": "Alibaba-NLP/gte-multilingual-base",
     "modernbert": "nomic-ai/modernbert-embed-base",
@@ -41,7 +54,7 @@ models_embedding = {
 MAX_NEW_TOKENS = 3000
 
 
-class GeminiHuggingFacePipeline:
+class GenericHuggingFacePipeline:
     def __init__(self, model):
         self.model = model
         self.pipeline = None
@@ -58,18 +71,38 @@ class GeminiHuggingFacePipeline:
 @timeit
 def load_model(model_alias="gemini-2.0-flash", max_new_tokens=MAX_NEW_TOKENS):
     print("load_model...")
-    model_name = models_text.get(model_alias, models_text["gemini-2.0-flash"])
-    print(f"{model_alias=} {model_name=} {max_new_tokens=}")
-    if model_name.startswith("gemini"):
+    model_data = models_text.get(model_alias, models_text["gemini-2.0-flash"])
+    print(f"{model_alias=} {max_new_tokens=} {model_data=}")
+
+    provider = model_data[0]
+    model_name = model_data[1]
+
+    if provider in ["open_router", "github"]:
+        print("Loading OpenRouter or Github model...")
+        open_router_key = os.environ["OPEN_ROUTER_API_KEY"]
+        github_key = os.environ["GITHUB_TOKEN"]
+        api_key = github_key if provider == "github" else open_router_key
+        base_url = model_data[2]
+        llm = ChatOpenAI(
+            model_name=model_name,
+            api_key=api_key,
+            base_url=base_url,
+            # max_new_tokens=max_new_tokens,
+            max_completion_tokens=max_new_tokens,
+        )
+        print(f"{llm=}")
+        hf_pipe = GenericHuggingFacePipeline(model=llm)
+    elif provider == "google":
         print("Loading ChatGoogleGenerativeAI model...")
         llm = ChatGoogleGenerativeAI(
             model=model_name,
             temperature=0,
-            max_new_tokens=max_new_tokens,
+            max_output_tokens=max_new_tokens,
             timeout=10,
             max_retries=2,
         )
-        llm = GeminiHuggingFacePipeline(model=llm)
+        print(f"{llm=}")
+        hf_pipe = GenericHuggingFacePipeline(model=llm)
     else:
         print("Loading transformers.pipeline local model...")
         pipe = transformers.pipeline(
@@ -83,9 +116,9 @@ def load_model(model_alias="gemini-2.0-flash", max_new_tokens=MAX_NEW_TOKENS):
             # truncation=True, do_sample=True,
             # top_k=50, top_p=0.95,
         )
-        llm = HuggingFacePipeline(pipeline=pipe)
-    print(f"{llm=}")
-    return llm, model_name, max_new_tokens
+        hf_pipe = HuggingFacePipeline(pipeline=pipe)
+    print(f"{hf_pipe=}")
+    return hf_pipe, model_name, max_new_tokens
 
 
 @timeit
